c87ba02b54caeee5fb2227ae9b8a1eea0d590238
jordanho2782 <jordanho2782@gmail.com>
2020-04-03 12:10:52 +0800

    Allowed_Hosts change from * to intense

--- fyp/settings.py
+++ fyp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['intense-earth-97794.herokuapp.com']
 
 
 # Application definition
